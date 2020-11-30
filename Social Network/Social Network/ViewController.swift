fd63c68e80a25cc3b15365da55fcaf6cdeff7611
Nikita <nik-demidyk@bk.ru>
2020-11-30 21:47:51 +1100

    Create new project

---
+++ Social Network/Social Network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Network
+//
+//  Created by Nikita Gras on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
